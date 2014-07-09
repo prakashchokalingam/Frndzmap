3a46036096247d2b4d2e85b2efa124a6ffb3d52a
prakashchokalingam <cpvicky33@gmail.com>
2014-07-09 14:33:57 +0530

    Update index.php

--- x/index.php
+++ x/index.php
@@ -2,8 +2,8 @@
 set_time_limit(0);
 echo '<link href="core.css" rel="stylesheet" />';
 $code=$_GET['code'];
-$appid='483472795069785';
-$appsec='612b75b67bb6b03fbf1bb0cdf2f18a28';
+$appid='xxxxxxxxxxxxxxx';
+$appsec='xxxxxxxxxxxxxxxxxx';
 $re='https://apps.facebook.com/483472795069785/x/';
 $a='https://graph.facebook.com/oauth/access_token?code='.$code.'&client_id='.$appid.'&client_secret='.$appsec.'&redirect_uri='.$re;
 $x=file_get_contents($a,true);
